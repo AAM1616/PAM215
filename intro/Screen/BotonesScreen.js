109097d3077ea48c96b86872d77e641b43463803
AAM1616 <124049914@upq.edu.mx>
2025-10-17 20:18:51 -0600

    Bottons & Switch
     Buttons y Switch Completada

--- intro/Screen/BotonesScreen.js
+++ intro/Screen/BotonesScreen.js
@@ -1,12 +1,65 @@
-import { Text, StyleSheet, View } from 'react-native'
+import { Text, StyleSheet, View, Button, Switch } from 'react-native'
+import React,{useState} from 'react';
+
 
 export default function BotonesScreen() {
-    return (
-      <View>
-        <Text>Proximamente por Daniel...</Text>
+  const [esEncendido, cambiarEncendido] = useState(false);
+  const [color, cambiarColor] = useState('yellow');
+  return (
+      <View style={styles.container}>
+        <Text style={styles.titulo}>Control de luz</Text>
+        <Text style={[styles.luz, {color: esEncendido ? color: 'black'}]}>
+          {esEncendido ? 'Luz encendida' : 'Luz apagada'}
+        </Text>
+        <Switch
+        value={esEncendido}
+        onValueChange={() => cambiarEncendido(!esEncendido)}
+        trackColor={{true: 'yellow', false: 'gray'}}
+        ></Switch>
+        <View style={styles.cajaBotones}>
+        <Button
+        title='Amarillo'
+        onPress={() =>esEncendido && cambiarColor('yellow')}
+        color= '#b4e83aff'
+        ></Button>
+        <Button
+        title='Azul'
+        onPress={() =>esEncendido && cambiarColor('blue')}
+        color= '#5829c7ff'
+        ></Button>
+                 
+        <Button
+        title='Rojo'
+        onPress={() =>esEncendido && cambiarColor('red')}
+        color='#e02c2cff'
+        ></Button>
+        </View>
+
       </View>
     )
   }
 
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, //
+    backgroundColor: '#000000ff', // color del fondo
+    alignItems: 'center', 
+    justifyContent: 'center', 
+  },
+  cajaBotones: {
+    flexDirection: 'row',
+    gap: 10,
+    marginTop: 15,
+  },
+  titulo: {
+    fontSize : 40,
+    color: 'white',
+    marginBottom: 20,
+    fontWeight: 'bold',
+  },
+  luz: {
+    fontSize: 30,
+    marginBottom: 15
+  },
+})
