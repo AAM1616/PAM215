38207a4f10591d4491c0dac9b563cb5143dada0a
AAM1616 <124049914@upq.edu.mx>
2025-10-28 20:29:36 -0600

    Activity Indicator
    Completado 👍

--- intro/Screen/ActivityScreen.js
+++ intro/Screen/ActivityScreen.js
@@ -1,12 +1,57 @@
-import { Text, StyleSheet, View } from 'react-native'
+import { Text, StyleSheet, View, ActivityIndicator, Button} from 'react-native'
+import React, { useState } from 'react'
 
-export default function ActivityScreen() {
-    return (
+export default function ActivityScreen () {
+
+
+    const [loading, setLoading] = useState (false);
+    const startLoading = () =>{
+      setLoading(true);
+      setTimeout(()=> setLoading(false), 3000)
+    };
+
+    if(loading){
+      return(
+      <View style={styles.container}>
+        <View style={styles.loadercontainer}>
+          <ActivityIndicator
+              size="large"
+              color="#008ed5ff"
+              animating={true}
+              hidesWhenStopped={true}
+          />
+          <Text style={styles.texto}>Cargando...</Text>
+        </View>
+      </View>
+      );
+    }
+    return(
       <View>
-        <Text>Proximamente por Leonel...</Text>
+        <Text style={styles.title} >ActivityIndicator</Text>
+        <Button
+        title="carga de datos" onPress={startLoading}/>
       </View>
     )
-  }
+}
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#000000ff',
+  },
+  texto: {
+    color: '#ffffffff',
+  },
+  title: {
+    fontSize: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 20,
+  },
+  loadercontainer: {
+    alignItems: 'center',
 
-const styles = StyleSheet.create({})
+}
+})
